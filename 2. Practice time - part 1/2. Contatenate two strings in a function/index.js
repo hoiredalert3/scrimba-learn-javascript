6c89a4a3138aa887d22617043261a9524033be79
John Doe <nphoi.toank28cltt@gmail.com>
2023-01-27 10:46:50 +0700

    built a simple calculator

--- 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
+++ 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
@@ -1,5 +1,10 @@
-let name = "Linda"
-let greeting = "Hi there"
+let name = "Linda";
+let greeting = "Hi there";
 
 // Create a function that logs out "Hi there, Linda!" when called
+function sayHello() {
+  let helloStr = greeting + ", " + name + "!";
+  console.log(helloStr);
+}
 
+sayHello();
--- 2. Practice time - part 1/5. Rendering an error message/index.js
+++ 2. Practice time - part 1/5. Rendering an error message/index.js
@@ -2,4 +2,12 @@
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
 
+// purchaseEl = document.getElementById("purchase-btn").onclick =
+//   "console.log('Error')";
+errorEl = document.getElementById("error");
 
+function showError() {
+  let errorStr = "Something went wrong, please try again";
+  errorEl.textContent = errorStr;
+  console.log("Show error");
+}
