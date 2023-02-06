64c72d4e30b7ffa5b206e2ab72d182a0a4d54e36
John Doe <nphoi.toank28cltt@gmail.com>
2023-02-07 00:23:11 +0700

    store and load leads from localStorage

--- 5. Build a Chrome Extension/33. Your first localStorage/index.js
+++ 5. Build a Chrome Extension/33. Your first localStorage/index.js
@@ -1,11 +1,25 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 // 1. Save a key-value pair in localStorage
 // 2. Refresh the page. Get the value and log it to the console
 // 3. Clear localStorage
+
+localStorage.setItem("myLead", "w3school.com");
+
+//localStorage.length returns the number of key/values pairs
+console.log(localStorage.length);
+// localStorage.key(n) return the nth key, null if n >= length
+console.log(localStorage.key(0));
+
+let myLead = localStorage.getItem("myLead");
+console.log(myLead);
+
+console.log(localStorage.removeItem("myLead"));
+
+// localStorage.clear();
 
 // HINTS:
 // localStorage.setItem(key, value)
@@ -13,23 +27,22 @@ const ulEl = document.getElementById("ul-el")
 // localStorage.clear()
 // PS: both key and value need to be strings
 
-
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
+});
 
 function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
                     ${myLeads[i]}
                 </a>
             </li>
-        `
-    }
-    ulEl.innerHTML = listItems  
+        `;
+  }
+  ulEl.innerHTML = listItems;
 }
