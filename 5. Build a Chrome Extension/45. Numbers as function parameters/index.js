b162e198e3ca2ebcac276f0e80481ac32a2bf073
John Doe <nphoi.toank28cltt@gmail.com>
2023-02-07 00:24:48 +0700

    refractor renderLeads() to use parameters

--- 5. Build a Chrome Extension/45. Numbers as function parameters/index.js
+++ 5. Build a Chrome Extension/45. Numbers as function parameters/index.js
@@ -1,6 +1,9 @@
 // Create a function, add(), that adds two numbers together and returns the sum
+function add(a, b) {
+  return a + b;
+}
 
+// const add = (a, b) => a + b;
 
-
-console.log( add(3,4)    ) // should log 7
-console.log( add(9, 102) ) // should log 111
+console.log(add(3, 4)); // should log 7
+console.log(add(9, 102)); // should log 111
--- 5. Build a Chrome Extension/47. Arrays as parameters/index.js
+++ 5. Build a Chrome Extension/47. Arrays as parameters/index.js
@@ -1,5 +1,13 @@
 // Create a function, getFirst(arr), that returns the first item in the array
+function getFirst(arr) {
+  return arr[0];
+}
 
+// const getFirst = (arr) => {
+//   return arr[0];
+// };
 
+// let getFirst = (arr) => arr[0];
 
 // Call it with an array as an argument to verify that it works
+console.log(getFirst([-4, 2]));
